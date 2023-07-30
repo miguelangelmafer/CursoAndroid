07d7e91a433835f5e5d0425f535f5158ac6aa465
miguelangelmafer <105967914+miguelangelmafer@users.noreply.github.com>
2023-07-30 16:16:45 +0200

    Update Constants.kt

--- WeatherApp/app/src/main/java/com/birdshoe/weatherapp/Constants.kt
+++ WeatherApp/app/src/main/java/com/birdshoe/weatherapp/Constants.kt
@@ -7,7 +7,7 @@ import android.os.Build
 
 object Constants {
 
-    const val APP_ID: String = "329c18a0b146e38862fa827a552e6672"
+    const val APP_ID: String = ""
     const val BASE_URL : String = "https://api.openweathermap.org/data/"
     const val METRIC_UNIT : String = "metric"
     const val PREFERENCE_NAME = "WeatherAppPreferences"
